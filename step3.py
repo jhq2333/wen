1a05bbe47771edc737088f41ee2a3fdc1954f5dc
jhq2333 <118903100+jhq2333@users.noreply.github.com>
2023-06-07 20:26:30 +0800

    Update step3.py

--- step3.py
+++ step3.py
@@ -3,4 +3,4 @@ import torch
 import wenetruntime as wenet
 # 下载https://github.com/wenet-e2e/wenet/releases/download/v2.0.1/chs.tar.gz
 # 并解压缩到/root/.wenet/chs目录下
-decoder = wenet.Decoder(model_dir="/root/.wenet/chs/chs/",lang='chs')
+decoder = wenet.Decoder(lang='chs')
